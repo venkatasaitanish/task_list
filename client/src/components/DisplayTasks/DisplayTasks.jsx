ae14929cd273eb7ae2358756a9b8b2ff79b0088b
Jonnadula Venkata Sai Tanish <venkatasaitanish@gmail.com>
2022-06-24 11:16:09 +0530

    Style update

--- client/src/components/DisplayTasks/DisplayTasks.jsx
+++ client/src/components/DisplayTasks/DisplayTasks.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
+import { CircularProgress, Card, CardContent, Typography } from '@mui/material';
 import axios from 'axios';
 import styles from './DisplayTasks.module.css';
-const local_url = 'http://localhost:5500/api/item';
+// const local_url = 'http://localhost:5500/api/item';
 const base_url = 'https://task-list-tanish.herokuapp.com/api/item';
 
 const DisplayTasks = () => {
 
     const [taskList, setTaskList] = useState([]);
+    const [fetch, setFetch] = useState(false);
 
     const deleteTask = async (id) => {
         try {
@@ -26,6 +28,7 @@ const DisplayTasks = () => {
                 const res = await axios.get(`${base_url}`);
                 console.log(res.data);
                 setTaskList(res.data);
+                setFetch(true);
             } catch (err) {
                 console.log(err);
             }
@@ -33,40 +36,68 @@ const DisplayTasks = () => {
         getallTasks();
     }, [taskList.values]);
 
+    const Loading = () => {
+        return(
+            <div className='text-center'>
+                <CircularProgress/>
+            </div>
+        )
+    }
+
+    const NoTasks = () => {
+        return(
+            <div className={styles.noTasks}>
+                <Card >
+                    <CardContent>
+                        <Typography variant='h5' component='div' className='text-center'>
+                            No Tasks To Do 🎆
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.content}>
             <div className='container'>
-                <h2 className='text-center'>Task List</h2>
+                <h1 className={`text-center ${styles.heading}`}>Task List</h1>
                 <div className={styles.addBtn}>
                     <Link to="/addTask">
                         <button className="btn btn-primary">Add Task</button>
                     </Link>
                 </div>
-                <div className={styles.table}>
-                    <Table responsive="sm" className="table-bordered">
-                        <thead>
-                            <tr className='text-center'>
-                                <th>Task</th>
-                                <th>Action</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {taskList.map((curr) => (
-                                <tr key={curr._id}>
-                                    <td>{curr.item}</td>
-                                    <td className='text-center'>
-                                        <Link to={`/updateTask/${curr._id}`}>
-                                            <button className="btn btn-info">Update</button>
-                                        </Link>
-                                    </td>
-                                    <td className='text-center'>
-                                        <button className="btn btn-danger" onClick={() => { deleteTask(curr._id) }}>Delete</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
+                <div>
+                    {fetch === false ? (Loading()) : taskList && taskList.length > 0 ? (
+                        <Card>
+                            <CardContent className={styles.tableCardContent}>
+                                <Table responsive="sm" className="table-bordered">
+                                    <thead>
+                                        <tr className='text-center'>
+                                            <th>Task</th>
+                                            <th>Action</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {taskList.map((curr) => (
+                                            <tr key={curr._id}>
+                                                <td className={styles.col}>{curr.item}</td>
+                                                <td className='text-center'>
+                                                    <Link to={`/updateTask/${curr._id}`}>
+                                                        <button className="btn btn-info">Update</button>
+                                                    </Link>
+                                                </td>
+                                                <td className='text-center'>
+                                                    <button className="btn btn-danger" onClick={() => { deleteTask(curr._id) }}>Delete</button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </Table>
+                            </CardContent>
+                        </Card>
+                    ) : (NoTasks())}
                 </div>
             </div>
         </div>
--- client/src/components/SubmitTask/SubmitTask.jsx
+++ client/src/components/SubmitTask/SubmitTask.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
+import {CircularProgress} from '@mui/material';
 import styles from './SubmitTask.module.css';
-const local_url = 'http://localhost:5500/api/item';
+// const local_url = 'http://localhost:5500/api/item';
 const base_url = 'https://task-list-tanish.herokuapp.com/api/item';
 
 const SubmitTask = () => {
 
     const[item, setItem] = useState('');
+    const[fetch, setFetch] = useState(false);
     const navigate = useNavigate();
     const { id } = useParams();
 
     const addTask = async (e) => {
         e.preventDefault();
         const newTask = {item};
-        if (id) {
+        if(id) {
             try {
                 const res = await axios.put(`${base_url}/${id}`, newTask);
                 console.log(res.data);
@@ -39,16 +41,30 @@ const SubmitTask = () => {
             try {
                 const res = await axios.get(`${base_url}/${id}`);
                 setItem(res.data.item);
+                setFetch(true);
             } catch (err) {
                 console.log(err);
             }
         }
-        if (id) getTask();
+        if(id) {
+            getTask();
+        }
+        else {
+            setFetch(true);
+        }
     }, [id]);
 
     const renderTitle = () => {
         if (id) return <h2 className="text-center my-2">Update Task</h2>
         return <h2 className="text-center my-2">Add Task</h2>
+    }
+
+    const Loading = () => {
+        return (
+            <div className='text-center'>
+                <CircularProgress />
+            </div>
+        )
     }
 
     return (
@@ -57,25 +73,27 @@ const SubmitTask = () => {
                 <div className="card col-md-6 offset-md-3">
                     {renderTitle()}
                     <div className="card-body">
-                        <form>
-                            <div className="form-group mb-2">
-                                <label className="form-label"> Task :</label>
-                                <input
-                                    type="text"
-                                    placeholder="Enter task"
-                                    name="task"
-                                    className="form-control"
-                                    value={item}
-                                    onChange={(e) => setItem(e.target.value)}
-                                />
-                            </div>
-                            <div>
-                                <button
-                                    className="btn btn-success" onClick={(e) => addTask(e)}>
-                                    Submit
-                                </button>
-                            </div>
-                        </form>
+                        {fetch === false ? (Loading()) : (
+                            <form>
+                                <div className="form-group mb-2">
+                                    <label className="form-label"> Task :</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Enter task"
+                                        name="task"
+                                        className="form-control"
+                                        value={item}
+                                        onChange={(e) => setItem(e.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <button
+                                        className="btn btn-success" onClick={(e) => addTask(e)}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        )}
                     </div>
                 </div>
             </div>
